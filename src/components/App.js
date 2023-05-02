a7813c38ebbb271bf4269d62d4e537e2e06eee4d
ramiroVelardez <ramiro.velardez01@gmail.com>
2023-05-02 16:25:56 -0300

    commit de prueba

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import SideBar from './SideBar';
+import SideBar from "./SideBar";
 
 function App() {
-  return (
-    <React.Fragment>
-      	<div id="wrapper">
-          <SideBar />
-        </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <div id="wrapper">
+                <SideBar />
+            </div>
+        </React.Fragment>
+    );
 }
 
 export default App;
